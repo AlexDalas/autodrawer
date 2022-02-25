4e26a849993e4b32afcc26ac453cba70ef0a28ef
Siydge <53163523+Siydge@users.noreply.github.com>
2022-02-25 19:32:01 +1100

    mmm, push

--- AutoDrawer Program/Class1.cs
+++ AutoDrawer Program/Class1.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
 
 namespace AutoDrawer
 {
     class HotkeySystem
     {
+        private IKeyboardMouseEvents m_GlobalHook;
 
+        public void Subscribe()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyPress += KeyPressed;
+        }
+
+        public static void KeyPressed(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        public void Unsubscribe()
+        {
+            m_GlobalHook.KeyPress -= KeyPressed;
+            m_GlobalHook.Dispose();
+        }
     }
 }
